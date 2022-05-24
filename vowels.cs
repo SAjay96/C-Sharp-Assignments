c6414bf65e9ee04423a8273f2bc7164304dbbd0d
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-25 00:13:47 +0530

    Create vowels.cs

---
+++ vowels.cs
@@ -0,0 +1,40 @@
+class vowels
+{
+	static void main(String[] args)
+	{
+
+		Console.WriteLine("Enter a character:");
+
+		char ch = Convert.ToChar(Console.Read());
+
+		if (ch == 'a' || ch == 'A')
+		{
+			Console.WriteLine("vowel");
+		}
+		else if (ch == 'e' || ch == 'E')
+		{
+			Console.WriteLine("vowel");
+		}
+		else if (ch == 'i' || ch == 'I')
+		{
+			Console.WriteLine("vowel");
+		}
+		else if (ch == 'o' || ch == 'O')
+		{
+			Console.WriteLine("vowel");
+		}
+		else if (ch == 'u' || ch == 'U')
+		{
+			Console.WriteLine("vowel");
+		}
+		else
+		{
+			Console.WriteLine("consonent");
+		}
+
+
+	}
+
+}
+
+
