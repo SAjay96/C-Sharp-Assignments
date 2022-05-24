0c74f8aa7f0fe2156eab27d6ede8b1b8667ed002
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-25 00:06:28 +0530

    Create stu.cs

---
+++ stu.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Student
+    {
+        public int RollNumber
+        { 
+            get;
+            set; 
+        }
+        public string Name 
+        {
+            get(value);
+            set(return Name); 
+        }
+        public int  percentage 
+        { 
+            get;
+            set; 
+        }
+    }
+}
