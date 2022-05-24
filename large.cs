4b1fcb1777d44cf042316af1d847c6e9e29f9daa
SAjay96 <97229211+SAjay96@users.noreply.github.com>
2022-05-25 00:18:25 +0530

    Create large.cs

---
+++ large.cs
@@ -0,0 +1,25 @@
+ class Largest
+{
+
+	 static void main(String[] args)
+	{
+		
+		Console.WriteLine("Enter a 1st number:");
+		int num1=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("enter a 2nd number");
+		int num2=Convert.ToInt32(Console.ReadLine());
+
+		if (num1 > num2)
+		{
+            Console.WriteLine(num1 + " " + "is largest number");
+		}
+		else
+		{
+			Console.WriteLine(num1 + " " + "is largest number");
+		}
+
+
+	}
+
+}
+ 
